239d9ac773273e5df8ad2f4adf3646fa20437007
pa-wallace-ndongwe <87383046+pa-wallace-ndongwe@users.noreply.github.com>
2023-10-31 10:50:54 +0000

    Update App.test.js

--- Patient.Dashboard/src/patient.dashboard.frontend/src/App.test.js
+++ Patient.Dashboard/src/patient.dashboard.frontend/src/App.test.js
@@ -1,12 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders patient dashboard', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Patient Dashboard/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
 test('renders clinics table', () => {
   render(<App />);
   const linkElement = screen.getByText(/Clinics/i);
